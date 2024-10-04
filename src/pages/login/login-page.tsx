189fb47a62863cf7b1c1555db07179b719e2314d
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2024-10-04 11:59:40 +0300

    add css to login page

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -1,7 +1,48 @@
+import { useState, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './login-page.module.css';
+
 function LoginPage() {
+    const navigate = useNavigate();
+
+    const [ loginValue, setloginValue ] = useState<string>('');
+    const [ passwordValue, setPasswordValue ] = useState<string>('');
+    // const [ passwordShown, setPasswordShown ] = useState<boolean>(true);
+    
+    function onSubmit(e: FormEvent) {
+        e.preventDefault();
+        console.log('submit');
+        navigate('/teams');
+    }
+
     return (
-        <div>
-            Страница логина
+        <div className={styles.content}>
+            <form className={styles.form} onSubmit={e => onSubmit(e)}>
+                <h3 className={styles.title}>Авторизация</h3>
+                <div className={styles.inputs}>
+                    <label className={styles.label}>Логин
+                        <input
+                            className={styles.input}
+                            type='text'
+                            placeholder='Введите логин или адрес эл.почты'
+                            onChange={(e) => setloginValue(e.target.value)}
+                            value={loginValue}
+                            name={'login'}
+                        />
+                    </label>
+                    <label className={styles.label}>Пароль
+                        <input
+                            className={styles.input}
+                            type='password'
+                            placeholder='Введите пароль'
+                            onChange={(e) => setPasswordValue(e.target.value)}
+                            value={passwordValue}
+                            name={'password'}
+                        />
+                    </label>
+                </div>
+                <button className={styles.button} type='submit' disabled={!loginValue || !passwordValue} >Войти</button>
+            </form>
         </div>
     );
 }
