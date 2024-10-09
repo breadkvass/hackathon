8c2370c21dd8d9cf188902d50b611afebb291b68
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2024-10-09 13:47:54 +0300

    added icons arrowUp, unWrap, caretDown & caretUp as react elements

--- src/components/icons/rollUp/rollUp.tsx
+++ src/components/icons/rollUp/rollUp.tsx
@@ -3,7 +3,7 @@ import styles from './rollUp.module.css';
 const RollUp = () => {
     return (
         <svg className={styles.arrow} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9 14L12 11L15 14" stroke="#42434B" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M9 14L12 11L15 14" stroke="#42434B" />
         </svg>
     )
 }
