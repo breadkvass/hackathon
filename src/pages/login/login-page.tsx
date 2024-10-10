e4ffa3d86531b4f3e5455b442b263917a8e50dff
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2024-10-10 14:46:54 +0300

    small fixes

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -1,8 +1,8 @@
 import { useState, FormEvent, useEffect, ChangeEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Header from '../../components/header/header';
 import EyeIcon from '../../assets/images/icons/eye-slash.svg';
 import styles from './login-page.module.css';
-import Header from '../../components/header/header';
 
 function LoginPage() {
     const navigate = useNavigate();
