c58d73bc07a1157efe5549a1a3a00042e19591a0
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2024-10-11 21:30:21 +0300

    added mocked data

---
+++ src/data/data.tsx
@@ -0,0 +1,106 @@
+export const dataDynamics = [
+    {
+      "skills": "Hard Skills",
+      "kvartal": "1 квартал",
+      "assessment": 2
+    },
+    {
+      "skills": "Hard Skills",
+      "kvartal": "2 квартал",
+      "assessment": 3
+    },
+    {
+      "skills": "Hard Skills",
+      "kvartal": "3 квартал",
+      "assessment": 4
+    },
+    {
+      "skills": "Hard Skills",
+      "kvartal": "4 квартал",
+      "assessment": 4
+    },
+    {
+      "skills": "Soft Skills",
+      "kvartal": "1 квартал",
+      "assessment": 3
+    },
+    {
+      "skills": "Soft Skills",
+      "kvartal": "2 квартал",
+      "assessment": 4
+    },
+    {
+      "skills": "Soft Skills",
+      "kvartal": "3 квартал",
+      "assessment": 4
+    },
+    {
+      "skills": "Soft Skills",
+      "kvartal": "4 квартал",
+      "assessment": 4
+    }
+];
+
+export const dataStress = [
+    { квартал: '1 кв', оценка: 0.5 },
+    { квартал: '2 кв', оценка: 1 },
+    { квартал: '3 кв', оценка: 1.5 },
+    { квартал: '4 кв', оценка: 2 }
+]
+
+export const dataSkillsAssessment = [
+  {
+    "skills": "Hard Skills",
+    "assessment": 4,
+    "person": "Акимов Роберт"
+  },
+  {
+    "skills": "Soft Skills",
+    "assessment": 4,
+    "person": "Акимов Роберт"
+  },
+
+  {
+    "skills": "Hard Skills",
+    "assessment": 2,
+    "person": "Волков Денис"
+  },
+  {
+    "skills": "Soft Skills",
+    "assessment": 3,
+    "person": "Волков Денис"
+  },
+  
+  {
+    "skills": "Hard Skills",
+    "assessment": 4,
+    "person": "Журавлев Максим"
+  },
+  {
+    "skills": "Soft Skills",
+    "assessment": 2,
+    "person": "Журавлев Максим"
+  },
+
+  {
+    "skills": "Hard Skills",
+    "assessment": 4,
+    "person": "Комарова Вера"
+  },
+  {
+    "skills": "Soft Skills",
+    "assessment": 3,
+    "person": "Комарова Вера"
+  },
+
+  {
+    "skills": "Hard Skills",
+    "assessment": 4,
+    "person": "Костин Михаил"
+  },
+  {
+    "skills": "Soft Skills",
+    "assessment": 4,
+    "person": "Костин Михаил"
+  },
+];
