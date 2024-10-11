e4ce958940c8dce8cf0801482d471b5875f6b0ae
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2024-10-11 21:34:13 +0300

    updated teamsSummaryContainer for universal use

--- src/components/teamsSummaryContainer/teamsSummaryContainer.tsx
+++ src/components/teamsSummaryContainer/teamsSummaryContainer.tsx
@@ -1,15 +1,28 @@
 import { FC } from 'react';
+// import Info from '../../assets/images/icons/info.svg';
+import Info from '../icons/info/info';
+import Badge from '../icons/badge/badge';
 import styles from './teamsSummaryContainer.module.css';
 
 type TeamsSummaryContainerProps = {
     number: number;
     type: string;
+    factor: boolean;
 }
 
-const TeamsSummaryContainer: FC<TeamsSummaryContainerProps> = ({number, type}) => {
+const TeamsSummaryContainer: FC<TeamsSummaryContainerProps> = ({number, type, factor}) => {
     return (
-        <div className={styles.sumContainer}>
+        <div className={styles.container}>
+            { !factor
+            ? 
             <p className={styles.number}>{number}</p>
+            :
+            <div className={styles.summary}>
+                <Badge style={styles.margin}/>
+                <p className={styles.number}>{number}</p>
+                <Info />
+            </div>
+            }
             <p className={styles.type}>{type}</p>
         </div>
     )
