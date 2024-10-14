8eb3dc4bdf885468bcfe3337a8e908255b5d5e5d
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2024-10-14 11:46:37 +0300

    added config provider ant design

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from 'react';
 import Assistant from '../assistant/assistant';
 import styles from './layout.module.css';
+import { ConfigProvider } from 'antd';
 
 type LayoutProps = {
     children: ReactNode;
@@ -8,10 +9,30 @@ type LayoutProps = {
 
 const Layout: FC<LayoutProps> = ({children}) => {
     return (
-        <div className={styles.layout}>
-            {children}
-            <Assistant />
-        </div>
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: "#42434B",
+                    fontFamily: "Manrope",
+                    fontSize: 16,
+                    lineHeight: 1.5,
+                },
+                components: {
+                    Tabs: {
+                        inkBarColor: "#E10D34",
+                        itemHoverColor: "#42434B",
+                        itemSelectedColor: "#42434B",
+                        itemColor: "#86878C",
+                        cardHeight: "42px"
+                    }
+                }
+            }}
+        >
+            <div className={styles.layout}>
+                {children}
+                <Assistant />
+            </div>
+        </ConfigProvider>
     )
 }
 
