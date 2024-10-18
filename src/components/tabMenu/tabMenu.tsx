cf038ffb761faebf735a37cfc05f2466e6775038
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2024-10-18 15:15:48 +0300

    update tabMenu

--- src/components/tabMenu/tabMenu.tsx
+++ src/components/tabMenu/tabMenu.tsx
@@ -1,29 +1,68 @@
-import { FC, MouseEventHandler } from 'react';
-import Tab from './tab/tab';
+import { FC, MouseEventHandler, useContext } from 'react';
 import styles from './tabMenu.module.css';
+import { TeamsContext } from '../../utils/teamsContext';
+import { useNavigate } from 'react-router-dom';
+import TeamsIcon from '../icons/teamsIcon/teamsIcon';
+import UserIcon from '../icons/userIcon/userIcon';
+import RollUp from '../icons/rollUp/rollUp';
+import UnWrapIcon from '../icons/unWrapIcon/unWrapIcon';
 
 type TabMenuProps = {
-    onTeamsClickHandler: MouseEventHandler;
+    onTeamsClickHandler: any;
     onEmployeesClickHandler: MouseEventHandler;
     onItemClickHandler: MouseEventHandler;
     selectedTab: 'teams' | 'employees' | '';
-    employee?: string
+    employee?: string;
 }
 
-const TabMenu: FC<TabMenuProps> = ({onTeamsClickHandler,onEmployeesClickHandler, onItemClickHandler, selectedTab}) => {
+const TabMenu: FC<TabMenuProps> = ({onTeamsClickHandler, onEmployeesClickHandler, onItemClickHandler, selectedTab}) => {
+    const [ teams ] = useContext(TeamsContext);
+    const navigate = useNavigate();
+    const arrow = selectedTab === selectedTab ? <RollUp /> : <UnWrapIcon />;
+    const style = selectedTab === selectedTab ? styles.button + ' ' + styles.selected : styles.button;
+
+    const onTeamClickHandler = (teamId: number) => {
+        onItemClickHandler;
+        navigate(`/teams/${teamId}`);
+    }
+
     return (
         <nav className={styles.sidebar}>
             <ul className={styles.list}>
-                <Tab tab={'teams'}
-                    onTabClickHandler={onTeamsClickHandler}
-                    selectedTab={selectedTab}
-                    onItemClickHandler={onItemClickHandler}
-                />
-                <Tab tab={'employees'}
-                    onTabClickHandler={onEmployeesClickHandler}
-                    selectedTab={selectedTab}
-                    onItemClickHandler={onItemClickHandler}
-                />
+                <li className={styles.element}>
+                    <button className={style} onClick={onTeamsClickHandler} name={'teams'}>
+                        <TeamsIcon />
+                        <p className={styles.name}>Команды</p>
+                        {arrow}
+                    </button>
+                    {selectedTab === 'teams' && teams &&
+                    <ul className={styles.items}>
+                        {teams.map((team, i) =>
+                            <li className={styles.item} key={i}>
+                                <button className={styles.button} onClick={() => onTeamClickHandler(team.id)}>
+                                    <p className={styles.name}>{team.name}</p>
+                                </button>
+                            </li>
+                        )}
+                    </ul>
+                    }                
+                </li>
+                <li className={styles.element}>
+                    <button className={style} onClick={onEmployeesClickHandler} name={selectedTab}>
+                        <UserIcon />
+                        <p className={styles.name}>Сотрудники</p>
+                        {arrow}
+                    </button>
+                    {/* {selectedTab === 'teams' && items &&
+                    <ul className={styles.items}>
+                        {items.map((item, i) =>
+                            <li className={styles.item} key={i}>
+                                <button className={styles.button} onClick={() => onItemClickHandler(item.id)}><p className={styles.name}>{item.name}</p></button>
+                            </li>
+                        )}
+                    </ul>
+                    }                 */}
+                </li>
             </ul>
         </nav>
     );
